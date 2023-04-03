2787ae4d54971223d8ed8c7e4f01decde0a9047a
alkumeidy <p.alkumeidy@gmail.com>
2023-04-03 11:54:04 -0600

    Created postgres DB

---
+++ server/migrations/20230403172818_create_movies.js
@@ -0,0 +1,20 @@
+const { HtmlTableImportModule } = require("react-tabulator/lib/types/TabulatorTypes");
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+    return knex.schema.createTable('movies', table => {
+        table.increments('id');
+        table.string('title');
+    })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+    return knex.schema.dropTableIfExists('movies');
+};
